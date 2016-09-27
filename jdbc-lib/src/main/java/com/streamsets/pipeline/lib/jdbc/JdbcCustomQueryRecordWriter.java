c7ed0bd5ceea163d0ef7b5cd51f9968a237e7f2d
SungMin Oh <springloops@gmail.com>
2016-09-27 13:11:06 +0900

    arrange exception

--- jdbc-lib/src/main/java/com/streamsets/pipeline/lib/jdbc/JdbcCustomQueryRecordWriter.java
+++ jdbc-lib/src/main/java/com/streamsets/pipeline/lib/jdbc/JdbcCustomQueryRecordWriter.java
@@ -7,9 +7,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -41,21 +39,10 @@ public class JdbcCustomQueryRecordWriter implements JdbcRecordWriter {
     public List<OnRecordErrorException> writeBatch(Collection<Record> batch) throws StageException {
         List<OnRecordErrorException> errorRecords = new LinkedList<>();
         try (Connection connection = dataSource.getConnection()) {
-            if (useMultiRowInsert) {
-                writeRecordByExecuteBatch(connection, batch);
-            } else {
-                writeRecordByQuery(connection, errorRecords, batch);
-            }
-
-        } catch (OnRecordErrorException error) {
-            errorRecords.add(error);
+            writeRecordByQuery(connection, errorRecords, batch);
         } catch (SQLException e) {
-            String formattedError = JdbcUtil.formatSqlException(e);
-            LOG.error(formattedError, e);
-            LOG.error("Query failed at: {}", System.currentTimeMillis());
-            throw new StageException(JdbcErrors.JDBC_14, formattedError);
+            handleSqlException(e);
         }
-
         return errorRecords;
     }
 
@@ -63,61 +50,81 @@ public class JdbcCustomQueryRecordWriter implements JdbcRecordWriter {
             Connection connection,
             List<OnRecordErrorException> errorRecords,
             Collection<Record> batch) throws StageException {
+
         Iterator<Record> iterator = batch.iterator();
-        Record record = null;
-        String query = null;
-        try (Statement stmt = connection.createStatement()) {
-            while (iterator.hasNext()) {
-                record = iterator.next();
-                query = record.get(JdbcUtil.CUSTOM_QUERY_FIELD_PATH).getValueAsString();
-                try {
-                    if (stmt.executeUpdate(query) == 1) {
-                        connection.commit();
-                    }
-                } catch (SQLException e) {
-                    LOG.error(JdbcErrors.JDBC_02.getMessage(), query, e);
-                    errorRecords.add(new OnRecordErrorException(record, JdbcErrors.JDBC_02, query, e.getMessage()));
-                }
-
-            }
-        } catch (SQLException e) {
-            LOG.error(JdbcErrors.JDBC_00.getMessage(), query, e);
-            throw new OnRecordErrorException(record, JdbcErrors.JDBC_00, query, e.getMessage());
-        }
-    }
-
-    private void writeRecordByExecuteBatch(Connection connection, Collection<Record> batch) throws StageException {
-
-        int size = batch.size();
-        Iterator<Record> iterator = batch.iterator();
-        Record record = null;
-        String query = null;
 
         try (Statement stmt = connection.createStatement()) {
             while (iterator.hasNext()) {
-                record = iterator.next();
-                query = record.get(JdbcUtil.CUSTOM_QUERY_FIELD_PATH).getValueAsString();
-                LOG.debug("Query String in Record : {}", query);
-
+                Record record = iterator.next();
+                String query = record.get(JdbcUtil.CUSTOM_QUERY_FIELD_PATH).getValueAsString();
                 stmt.addBatch(query);
             }
 
             try {
-                int[] result = stmt.executeBatch();
-                if (size == result.length) {
-                    connection.commit();
-                } else {
-                    LOG.error(JdbcErrors.JDBC_14.getMessage(), query);
-                    throw new OnRecordErrorException(record, JdbcErrors.JDBC_14, query);
-                }
+                stmt.executeBatch();
             } catch (SQLException e) {
-                LOG.error(JdbcErrors.JDBC_02.getMessage(), query, e);
-                throw new OnRecordErrorException(record, JdbcErrors.JDBC_02, query, e.getMessage());
+                if (rollbackOnError) {
+                    connection.rollback();
+                }
+                handleBatchUpdateException(batch, e, errorRecords);
             }
+            connection.commit();
         } catch (SQLException e) {
-            // Exception executing query
-            LOG.error(JdbcErrors.JDBC_00.getMessage(), query, e);
-            throw new OnRecordErrorException(record, JdbcErrors.JDBC_00, query, e.getMessage());
+            handleSqlException(e);
+        }
+    }
+
+    /**
+     * This is an error that is not due to bad input record and should throw a StageException
+     * once we format the error.
+     * @param e SQLException
+     * @throws StageException
+     */
+    private void handleSqlException(SQLException e) throws StageException {
+        String formattedError = JdbcUtil.formatSqlException(e);
+        LOG.error(formattedError);
+        LOG.debug(formattedError, e);
+        throw new StageException(JdbcErrors.JDBC_14, formattedError);
+    }
+
+    /**
+     * <p>
+     *   Some databases drivers allow us to figure out which record in a particular batch failed.
+     * </p>
+     * <p>
+     *   In the case that we have a list of update counts, we can mark just the record as erroneous.
+     *   Otherwise we must send the entire batch to error.
+     * </p>
+     * @param failedRecords List of Failed Records
+     * @param e BatchUpdateException
+     * @param errorRecords List of error records for this batch
+     */
+    private void handleBatchUpdateException(
+            Collection<Record> failedRecords, SQLException e, List<OnRecordErrorException> errorRecords
+    ) throws StageException {
+        if (JdbcUtil.isDataError(connectionString, e)) {
+            String formattedError = JdbcUtil.formatSqlException(e);
+            LOG.error(formattedError);
+            LOG.debug(formattedError, e);
+
+            if (!rollbackOnError && e instanceof BatchUpdateException &&
+                    ((BatchUpdateException) e).getUpdateCounts().length > 0) {
+                BatchUpdateException bue = (BatchUpdateException) e;
+
+                int i = 0;
+                for (Record record : failedRecords) {
+                    if (i >= bue.getUpdateCounts().length || bue.getUpdateCounts()[i] == PreparedStatement.EXECUTE_FAILED) {
+                        errorRecords.add(new OnRecordErrorException(record, JdbcErrors.JDBC_14, formattedError));
+                    }
+                    i++;
+                }
+            } else {
+                for (Record record : failedRecords) {
+                    errorRecords.add(new OnRecordErrorException(record, JdbcErrors.JDBC_14, formattedError));
+                }
+            }
+        } else {
+            handleSqlException(e);
         }
     }
 }
